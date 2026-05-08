c1f9e1d1b71dae704d1f09fe6184913716274e55
Bishwadip <bishwadipmaitra50@gmail.com>
2026-05-08 13:37:08 +0600

    Create utils for API Errors Response and Async Handler:

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, success, message, data) {
+        this.statusCode = statusCode;
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
